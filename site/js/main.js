79dc57202a141ab480d1a53b03fcf80beeeae615
Eric Lathrop <eric@ericlathrop.com>
2014-05-18 15:52:06 -0400

    Make hover gif only need data-hover-src attribute.

--- site/js/main.js
+++ site/js/main.js
@@ -1,8 +1,9 @@
 $(document).ready(function() {
-	$('.hoverGif').mouseenter(function() {
-		$(this).attr('src', $(this).data("hover"));
-	});
-	$('.hoverGif').mouseleave(function() {
-		$(this).attr('src', $(this).data("orig"));
+	$('img[data-hover-src]').each(function() {
+		$(this).data('orig-src', $(this).attr('src'));
+	}).mouseenter(function() {
+		$(this).attr('src', $(this).data("hover-src"));
+	}).mouseleave(function() {
+		$(this).attr('src', $(this).data("orig-src"));
 	});
 });
