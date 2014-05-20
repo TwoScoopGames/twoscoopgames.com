3e4470d349155c80654857b5a1b9345d5ee9af12
Eric Lathrop <eric@ericlathrop.com>
2014-05-19 22:14:16 -0400

    Make each-row work on objects.

--- helpers/each-row.js
+++ helpers/each-row.js
@@ -1,4 +1,4 @@
-module.exports = function(rowSize, context, options) {
+function eachRowArray(rowSize, context, options) {
 	var ret = "";
 	for (var i = 0; i < context.length; i += rowSize) {
 		var amt = Math.min(context.length - i, rowSize);
@@ -6,4 +6,30 @@ module.exports = function(rowSize, context, options) {
 		ret += options.fn(row);
 	}
 	return ret;
+}
+
+function eachRowObject(rowSize, context, options) {
+	var ret = "";
+	var keys = Object.keys(context).filter(function(key) {
+		return context.hasOwnProperty(key);
+	});
+	for (var i = 0; i < keys.length; i += rowSize) {
+		var amt = Math.min(keys.length - i, rowSize);
+		var row = keys.slice(i, i + amt).map(function(key) {
+			return [key, context[key]];
+		}).reduce(function(obj, val) {
+			obj[val[0]] = val[1];
+			return obj;
+		}, {});
+		ret += options.fn(row);
+	}
+	return ret;
+}
+
+module.exports = function(rowSize, context, options) {
+	if (Array.isArray(context)) {
+		return eachRowArray(rowSize, context, options);
+	} else {
+		return eachRowObject(rowSize, context, options);
+	}
 };
